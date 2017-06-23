a9860c4c86978e7e56242fb1e84d24fb183f3229
PRIYA-AGRAWAL-21 <priyaagrawalfeb@gmail.com>
2017-06-24 01:20:14 +0530

    Add files via upload

---
+++ AptronProjectGuardian.java
@@ -0,0 +1,34 @@
+package com.priya.cs.aptronproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AptronProjectGuardian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aptron_project_guardian);
+        Button b1 = (Button) findViewById(R.id.b2);
+        final EditText et2 = (EditText) findViewById(R.id.et2);
+        final EditText et3 = (EditText) findViewById(R.id.et3);
+        final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (et2.getText().toString().length() != 10) {
+                    et2.setError("Enter a valid phone number");
+
+                }
+
+                final String email = et3.getText().toString().trim();
+                if (!(email.matches(emailPattern)))
+                    et3.setError("Invalid Email Address");
+            }
+        });
+    }}
